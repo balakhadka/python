cba9ac20a6dae001b8985dd35d4ac05a07f85c98
balakhadka <45382278+balakhadka@users.noreply.github.com>
2018-11-27 12:15:48 +0545

    Add files via upload

---
+++ bala.py
@@ -0,0 +1,13 @@
+name = "sam"
+last_letters = name[::]
+l = last_letters 
+print (l)
+l = 'p' + l
+print (l)
+x = 'hello world'
+x = x + "it is bala!"
+print (x)
+x1 = "bala is beautiful"
+x1= x1. split('b')
+print (x1)
+
